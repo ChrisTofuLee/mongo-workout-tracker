169d99cd76b1e5a9371e715d0ea3dd116205459b
Selfay <chris.lee90@hotmail.co.uk>
2020-08-09 17:47:43 +0100

    adding in static path

--- Develop/routes/html.js
+++ Develop/routes/html.js
@@ -3,19 +3,20 @@ const express = require("express");
 const router = express.Router();
 
 const homePage = (req, res) => {
-  res.sendFile("public/index.html");
+  res.sendFile("index.html");
 };
 
 const exercisePage = (req, res) => {
-  res.sendFile("public/exercise.html");
+  res.sendFile("exercise.html");
 };
 
 const statsPage = (req, res) => {
-    res.sendFile("public/stats.html");
-  };
+  res.sendFile("stats.html");
+};
 
 router.get("/", homePage);
 router.get("/stats", statsPage);
 router.get("/exercise", exercisePage);
 
 module.exports = router;
+
