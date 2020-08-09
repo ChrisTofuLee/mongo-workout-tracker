44575edef9160eca3ae830324f69e3903e66cf2c
Selfay <chris.lee90@hotmail.co.uk>
2020-08-09 17:54:35 +0100

    adding in static path

--- Develop/server.js
+++ Develop/server.js
@@ -1,25 +1,28 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const htmlRoutes = require("./routes/html");
+const apiRoutes = require("./routes/api");
+const path = require("path")
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "./public")));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
-app.use(require("./routes/api.js"));
-app.use(require("./routes/html.js"));
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`App running on port http://localhost:${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT} !`);
+
 });
